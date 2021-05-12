2e1730af3c9391b7cec227806eb01d1bb4b2c5a3
jpartemis <john.parkhill@gmail.com>
2021-05-12 08:47:38 -0500

    app up

---
+++ cwarp_app.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np
+from datetime import date
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import yfinance as yf
+yf.pdr_override()
+import pandas as pd
+import streamlit as st
+
+def main():
+    st.sidebar.header("CWarp")
+
+main()
